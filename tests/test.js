49c39b001e3e045c8464352bf58d2acb23272189
Chad Francis <discgolfer1138@gmail.com>
2013-10-03 16:42:01 -0600

    started working on infrared control, improved test flow

--- tests/test.js
+++ tests/test.js
@@ -6,29 +6,44 @@ prompt.start();
 var prompt_options = [
 	{
 		'name':'host',
-		'default':'192.168.1.2'
+		// 'default':'192.168.1.2'
+		'default':'pointblank.x.chadfrancis.com'
 	},
 	{
 		'name':'port',
-		'default':'88'
+		// 'default':'88'
+		'default':'8111'
 	},
 	{
 		'name':'user',
 		'default':'admin'
 	},
-	'pass',
-	{
-		'name':'preset',
-		'default':'center'
-	}
+	'pass'
 ];
 
 prompt.get(prompt_options, function (err, result) {
-	var cam = new FoscamHdClient({host:result.host, port:result.port, user:result.user, pass:result.pass});
+	var cam = new FoscamHdClient({host:result.host, port:result.port, user:result.user, pass:result.pass}),
+		displayOutput = function(err, data){
+			if(err) throw err;
+			console.log(data);
+		};
 
-	cam.gotoPreset( result.preset, function(err, data){
-		if(err) return console.log(err);
-		console.log(data);
+	prompt.get([ {'name':'function', 'default':'gotoPreset'} ] , function (err, result){
+		switch(result.function){
+			case 'gotoPreset':
+				prompt.get([ {'name':'name', 'default':'center'} ], function (err, result){
+					cam.gotoPreset( result.name, displayOutput );
+				});
+				break;
+			case 'setInfrared':
+				prompt.get([ {'name':'state', 'default':'0'} ], function (err, result){
+					cam.setInfrared( result.state, displayOutput );
+				});
+				break;
+			default:
+				throw 'unsupported function';
+		}
 	});
+
 });
 
