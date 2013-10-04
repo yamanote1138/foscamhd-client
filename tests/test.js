6fb66fed7cd3fbc1c74f05e88cdd3d20e42999e7
Chad Francis <discgolfer1138@gmail.com>
2013-10-04 14:31:40 -0600

    tidying up some functions, added get/set ir mode. improved tests

--- tests/test.js
+++ tests/test.js
@@ -1,49 +1,43 @@
+//requires
 var FoscamHdClient = require('../lib/foscamhd.js'),
+	nconf = require('nconf'),
 	prompt = require('prompt');
+
+//setup
+var config = nconf.file('./tests/config.json').get('testcamera'),
+	client = new FoscamHdClient(config);
+
+var displayOutput = function(err, data){
+	if(err) throw err;
+	console.log(data);
+};
 
 prompt.start();
 
-var prompt_options = [
-	{
-		'name':'host',
-		// 'default':'192.168.1.2'
-		'default':'pointblank.x.chadfrancis.com'
-	},
-	{
-		'name':'port',
-		// 'default':'88'
-		'default':'8111'
-	},
-	{
-		'name':'user',
-		'default':'admin'
-	},
-	'pass'
-];
-
-prompt.get(prompt_options, function (err, result) {
-	var cam = new FoscamHdClient({host:result.host, port:result.port, user:result.user, pass:result.pass}),
-		displayOutput = function(err, data){
-			if(err) throw err;
-			console.log(data);
-		};
-
-	prompt.get([ {'name':'function', 'default':'gotoPreset'} ] , function (err, result){
-		switch(result.function){
-			case 'gotoPreset':
-				prompt.get([ {'name':'name', 'default':'center'} ], function (err, result){
-					cam.gotoPreset( result.name, displayOutput );
-				});
-				break;
-			case 'setInfrared':
-				prompt.get([ {'name':'state', 'default':'0'} ], function (err, result){
-					cam.setInfrared( result.state, displayOutput );
-				});
-				break;
-			default:
-				throw 'unsupported function';
-		}
-	});
-
+prompt.get([ {name:'function', description:'choose function to execute', 'default':'gotoPreset'} ] , function (err, result){
+	switch(result.function){
+		case 'gotoPreset':
+			prompt.get([ {'name':'name', 'default':'center'} ], function (err, result){
+				client.gotoPreset( result.name, displayOutput );
+			});
+			break;
+		case 'getIrMode':
+			client.getIrMode( displayOutput );
+			break;
+		case 'setIrMode':
+			prompt.get([ {'name':'mode', 'default':'manual'} ], function (err, result){
+				client.setIrMode( result.mode, displayOutput );
+			});
+			break;
+		case 'setIrState':
+			prompt.get([ {'name':'state', 'default':'on'} ], function (err, result){
+				client.setIrState( result.state, displayOutput );
+			});
+			break;
+		case 'getStreamingUrl':
+			client.getStreamingUrl( displayOutput );
+			break;
+		default:
+			throw 'unsupported function';
+	}
 });
-
